1ac439bdbdd50ee5a5b8464a3c6e622f59559c74
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2021-12-05 17:31:04 +0100

    add cli

---
+++ cli/src/cli.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:colorize/colorize.dart';
+import 'package:parser/parser.dart';
+
+Future<void> main(List<String> arguments) async {
+  exitCode = 0;
+  final parser = ArgParser()..addFlag('path', negatable: false, abbr: 'p');
+
+  ArgResults argResults = parser.parse(arguments);
+  final path = argResults.rest.first;
+
+  print(path);
+
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+    exitCode = 2;
+  } else {
+    final file = File(path);
+
+    MDParserBLoC parser = MDParserBLoC();
+    final model = await parser.parse(file: file);
+    final fullCode = _getFullCode(model);
+    final colorizedCode = Colorize(fullCode)..lightCyan();
+    stdout.writeln(colorizedCode);
+  }
+}
+
+String _getFullCode(ExerciseModel model) {
+  final codeBeforeSeed = model.beforeSeed?.code;
+  final seedCode = model.seed?.code;
+  final codeAfterSeed = model.afterSeed?.code;
+
+  final codeBeforeHints = model.codeBeforeAsserts?.code;
+  final codeAfterHints = model.codeAfterAsserts?.code;
+  final allHints = model.asserts?.map((a) => a.unitTest).join("\n");
+  String fullSourceCode = "";
+  if (codeBeforeSeed != null) {
+    fullSourceCode += '$codeBeforeSeed\n';
+  }
+  if (seedCode != null) {
+    fullSourceCode += '$seedCode\n';
+  }
+
+  if (codeAfterSeed != null) {
+    fullSourceCode += '\n$codeAfterSeed';
+  }
+
+  if (codeBeforeHints != null) {
+    fullSourceCode += '\n$codeBeforeHints';
+  }
+  if (allHints != null) {
+    fullSourceCode += '\n$allHints';
+  }
+  if (codeAfterHints != null) {
+    fullSourceCode += '\n$codeAfterHints';
+  }
+  return fullSourceCode;
+}
