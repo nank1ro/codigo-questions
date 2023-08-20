788a64867e0b670b9ba89c9dbc1467ffb4f120d7
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2023-08-20 16:51:22 +0200

    chore: regenerate parser model

--- parser/lib/src/blocs/models/assert_model/model.g.dart
+++ parser/lib/src/blocs/models/assert_model/model.g.dart
@@ -9,11 +9,15 @@ part of 'model.dart';
 _$_AssertModel _$$_AssertModelFromJson(Map<String, dynamic> json) =>
     _$_AssertModel(
       description: json['description'] as String,
-      unitTest: json['unitTest'] as String,
+      unitTest: json['unitTest'] as String?,
+      regexAssert: json['regexAssert'] == null
+          ? null
+          : RegexAssert.fromJson(json['regexAssert'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_AssertModelToJson(_$_AssertModel instance) =>
     <String, dynamic>{
       'description': instance.description,
       'unitTest': instance.unitTest,
+      'regexAssert': instance.regexAssert,
     };
